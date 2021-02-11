1b40896baf8b1fb2cd77a49dce8dc7ab9e4e22a3
MrGDS2 <mrsonicthunder@gmail.com>
2021-02-11 14:30:01 -0500

    responsive

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,21 +8,22 @@ const LandingPage = () => {
 
     return (
         <React.Fragment>
-          
-                <div className="main-page mb-5" id="home">
+
+            <div className="main-page mb-5" id="home">
                 <img src={Electrican} alt="landing" className="main-img" />
-                
-            <div className="centered">
-              <p className="company-name"> KCElectricalContractorLLC</p>
-            
-              <div className="centered-below">
-             <p className="company-tag">Company is Insured</p> 
-              <div>
-                 <span> <button className="centered-btn company-btn">Request An Estimate</button> </span>
-              </div>
-              </div>
-          </div>
-           </div>
+              <div className="align-title-center">
+                <div className="centered">
+                    <p className="company-name"> KCElectricalContractorLLC</p>
+
+                    <div className="centered-below">
+                        <p className="company-tag">Company is Insured</p>
+                        <div>
+                            <span> <button className="centered-btn company-btn">Request An Estimate</button> </span>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 
