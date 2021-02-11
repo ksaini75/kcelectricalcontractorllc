acfba64200c192d44a81c2ae8b863e7956627ffc
MrGDS2 <mrsonicthunder@gmail.com>
2021-02-11 00:03:40 -0500

    mobile landpage work

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,19 +1,28 @@
+import { Button } from 'reactstrap';
 import React from 'react';
-import Electrican from '../../Images/lightbulbs_cut.png';
+import Electrican from '../../Images/lightbulbs_cut_dark.png';
+import './LandingPage.scss';
 
 const LandingPage = () => {
 
 
     return (
         <React.Fragment>
-
-            <div className="main-page mb-5" id="home">
-             <img src={Electrican} height="420" alt="lamding"/>
-              <div className="centered company-name">
-              kcelectricalcontractorllc
+          
+                <div className="main-page mb-5" id="home">
+                <img src={Electrican} alt="landing" className="main-img" />
+                
+            <div className="centered">
+              <p className="company-name"> KCElectricalContractorLLC</p>
+            
+              <div className="centered-below">
+             <p className="company-tag">Company is Insured</p> 
+              <div>
+                 <span> <button className="centered-btn company-btn">Request An Estimate</button> </span>
               </div>
-            </div>
-    
+              </div>
+          </div>
+           </div>
         </React.Fragment>
     );
 
