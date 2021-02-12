765c6177d7913efaf10b4320eb3c793ea23190c1
MrGDS2 <mrsonicthunder@gmail.com>
2021-02-12 00:18:52 -0500

    nav fix

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
-import logo from '/Users/karishmas/Documents/Github/KCElectricalContractorLLC/kcelectricalcontractorllc/src/Images/ken-logo.png';
-
-
+import logo from '../../Images/ken-logo.png';
 
 
 const NavigationBar = () => (
    
         <Navbar expand="lg" bg="dark" className="nav">
-         <Image className='ken-logo' src='
-https://firebasestorage.googleapis.com/v0/b/kcelectricalcontractorll.appspot.com/o/Photos%2Flogo-clear.png?alt=media&token=ffb29dbe-5bd2-4c9e-acf5-c029e857c456'/>
+         <Image className='ken-logo' src={logo}/>
             <Navbar.Brand className="nav-title" href="/home">KCElectricalcontractorLLC</Navbar.Brand>
            
             <Navbar.Toggle aria-controls="basic-navbar-nav" className='custom-toggler-icon'/>
