b328a7590988bd76258d41c57eef104d56238d76
Karishma Saini <ksaini7595@gmail.com>
2021-03-01 16:35:43 -0500

    fixed the style and the sytnax for the a and p tags

--- src/components/FooterPage/Footer.js
+++ src/components/FooterPage/Footer.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { FaInstagram} from 'react-icons/fa';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './Footer.scss';
 
 class Footer extends Component {
@@ -7,31 +9,32 @@ class Footer extends Component {
         return (
 
         <div className = "footer_bg pt-5 pb-3  text-center" id = "footer">
-            <div className = "row">
-                <div className = " col-md-6  mr-auto">
-                    <p> <a className = "insta-icon"
-                    href = "https://www.instagram.com/kcelectricalcontractorllc/?igshid=13e62ll0r2ahi"
-                    target = "_blank"
-                    rel = "noopener noreferrer">
+            <Row>
+                <Col md={6} className = "mr-auto">
+                    <p> 
+                    <a className = "insta-icon" href = "https://www.instagram.com/kcelectricalcontractorllc/?igshid=13e62ll0r2ahi" target = "_blank" rel = "noopener noreferrer">
                     <FaInstagram />
-                    </a> </p> 
-                </div>
+                    </a> 
+                    </p> 
+                </Col>
 
-        <div className = "col-md-6 ml-auto">
-            <p className = 'footer-email'> KCelectrialcontractorllc @gmail.com </p>
-        </div>
+        <Col  md={6} className = "ml-auto">
+            <p className = 'footer-email'> KCelectrialcontractorllc@gmail.com </p>
+        </Col>
 
-         </div>
-            <div className = "row">
-                <div className = "col-md-12">
+         </Row>
+
+
+            <Row>
+                <Col md={12}>
                     <span className = "text-center">
                     <p className = "copyright"> ©2021 by <br/>
                     <a href = "https://github.com/ksaini75"> Karishma Saini </a><br/>
                     <a href = "https://github.com/MrGDS2"> MrGDS2 </a>
                     </p>
                     </span> 
-                </div> 
-            </div>
+                </Col> 
+            </Row>
         </div>
         )
     }
