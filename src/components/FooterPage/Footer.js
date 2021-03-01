ffe19d0a75ba36a51f59131f85abfcdfc9796cba
Karishma Saini <ksaini7595@gmail.com>
2021-03-01 11:59:38 -0500

    removed the instagram SVG image since I decided to use the react-icons instead.

--- src/components/FooterPage/Footer.js
+++ src/components/FooterPage/Footer.js
@@ -1,9 +1,5 @@
-import React, {
-    Component
-} from 'react';
-import {
-    FaInstagram
-} from 'react-icons/fa';
+import React, {Component} from 'react';
+import { FaInstagram} from 'react-icons/fa';
 import './Footer.scss';
 
 class Footer extends Component {
