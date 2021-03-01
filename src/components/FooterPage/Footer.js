54dcb738a5fed4d7295f0fdc35a6990a9b1b967f
Karishma Saini <ksaini7595@gmail.com>
2021-03-01 11:56:22 -0500

    completed the footer section

--- src/components/FooterPage/Footer.js
+++ src/components/FooterPage/Footer.js
@@ -1,52 +1,40 @@
-import React, {Component} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {FaInstagram } from 'react-icons/fa';
+import React, {
+    Component
+} from 'react';
+import {
+    FaInstagram
+} from 'react-icons/fa';
 import './Footer.scss';
 
+class Footer extends Component {
+    render() {
+        return (
 
+        <div className = "footer_bg pt-5 pb-3  text-center" id = "footer">
+            <div className = "row">
+                <div className = " col-md-6  mr-auto">
+                    <p> <a className = "insta-icon"
+                    href = "https://www.instagram.com/kcelectricalcontractorllc/?igshid=13e62ll0r2ahi"
+                    target = "_blank"
+                    rel = "noopener noreferrer">
+                    <FaInstagram />
+                    </a> </p> 
+                </div>
 
-class Footer extends Component{
-    render(){
-        return(
+        <div className = "col-md-6 ml-auto">
+            <p className = 'footer-email'> KCelectrialcontractorllc @gmail.com </p>
+        </div>
 
-<div className="footer_bg pt-5 pb-3  text-center" id="footer">
-  <div className="container">
-     <div className="row">
-        
-        <div className=" col-md-2">
-        <p> <FaInstagram className="insta-icon" /> <a className="icon-color" href="https://www.linkedin.com/in/erika-moya-82a608165/" target = "_blank" rel = "noopener noreferrer" >
-        </a>
-                 </p> 
-           
-        
-            
-            </div>
-            
-            <div className='col-md-8'>
-      
-                 </div>
-
-
-            <div className="col-md-2  ">
-            <p classname='footer-email'>KCelectrialcontractorllc@gmail.com</p></div>
-
-             </div>
-
-        <div className="row">
-            <div className="col-md-12 mt-4">
-            <span className="text-center">
-            <p className="copyright"> ©2021 by 
-            <br/>
-            <a href="https://github.com/ksaini75">Karishma Saini</a> 
-            <br/>
-
-            <a href="https://github.com/MrGDS2">MrGDS2</a>
-            
-            </p>
-
-            </span>
-            </div>
-            </div>
+         </div>
+            <div className = "row">
+                <div className = "col-md-12">
+                    <span className = "text-center">
+                    <p className = "copyright"> ©2021 by <br/>
+                    <a href = "https://github.com/ksaini75"> Karishma Saini </a><br/>
+                    <a href = "https://github.com/MrGDS2"> MrGDS2 </a>
+                    </p>
+                    </span> 
+                </div> 
             </div>
         </div>
         )
